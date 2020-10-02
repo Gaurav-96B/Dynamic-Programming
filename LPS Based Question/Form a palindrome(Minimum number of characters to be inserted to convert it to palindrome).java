01a10e67914b0fb049c32aed4cca75f81b9b8c71
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2020-10-02 23:05:53 +0530

    Update Form a palindrome(Minimum number of characters to be inserted to convert it to palindrome).java

--- LPS Based Question/Form a palindrome(Minimum number of characters to be inserted to convert it to palindrome).java
+++ LPS Based Question/Form a palindrome(Minimum number of characters to be inserted to convert it to palindrome).java
@@ -1,1 +1,52 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
+class GFG {
+    static int table[][]=new int[41][41];
+    static int lps(String s,int n,int m)
+    {
+        if(n>m)
+        {
+            return 0;
+        }
+        if(n==m)
+        {
+            return 1;
+        }
+        if(table[n][m]!=-1)
+        {
+            return table[n][m];
+        }
+        else if(s.charAt(n)==s.charAt(m))
+        {
+            return table[n][m]=2+lps(s,n+1,m-1);
+        }
+        else
+        {
+            return table[n][m]= Math.max(lps(s,n+1,m),lps(s,n,m-1));
+        }
+    }
+	public static void main (String[] args) {
+	    int t;
+	    Scanner sc=new Scanner(System.in);
+	    t=sc.nextInt();
+	    sc.nextLine();
+	    while(t-->0)
+	    {
+	        String s=sc.nextLine();
+	        int n=s.length();
+	        for(int i=0;i<=n;i++)
+	        {
+	            for(int j=0;j<=n;j++)
+	            {
+	                table[i][j]=-1;
+	            }
+	        }
+	        int z=lps(s,0,n-1);
+	        System.out.println(n-z);
+	    }
+	    
+	   
+	}
+}
