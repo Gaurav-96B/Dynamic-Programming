97e6c4c195879e5ff9abf8d1ed973e0300ef6c8c
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2021-04-14 10:07:00 +0530

    Update Longest Increasing Subsequence.java

--- LIS Based Question/Longest Increasing Subsequence.java
+++ LIS Based Question/Longest Increasing Subsequence.java
@@ -1,44 +1,27 @@
-class GFG {
-	public static void main (String[] args) {
-	    int t;
-	    Scanner sc=new Scanner(System.in);
-	    t=sc.nextInt();
-	    sc.nextLine();
-	    while(t-->0)
-	    {
-	        int n=sc.nextInt();
-	        int a[]=new int[n];
-	        for(int i=0;i<n;i++)
-	        {
-	            a[i]=sc.nextInt();
-	        }
-	        int table[]=new int[n];
-	        table[0]=1;
-	        for(int i=1;i<n;i++)
-	        {
-	            table[i]=1;
-	           for(int j=i-1;j>=0;j--) 
-	           {
-	             if(a[i]>a[j])
-	             {
-	               int possible=table[j]+1;
-	               if(possible>table[i])
-	               {
-	                  table[i]=possible;
-	               }
-	             }
-	           }
-	        }
-	           int max=0;
-	           for(int i=0;i<n;i++)
-	           {
-	              if(table[i]>max) 
-	              {
-	                max=table[i];  
-	              }
-	           }
-	           System.out.println(max);
-	        
-	    }
-	}
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int lis[]=new int[nums.length];
+        Arrays.fill(lis,1);
+        return LIS(nums,lis);   
+    }
+    public int LIS(int nums[],int lis[])
+    {
+       for(int i=1;i<nums.length;i++) 
+       {
+           for(int j=i-1;j>=0;j--)
+           {
+               if(nums[i]>nums[j])
+               {
+                   lis[i]=Math.max(lis[i],lis[j]+1);
+               }
+           }
+       }
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<lis.length;i++)
+        {
+            max=Math.max(max,lis[i]);
+        }
+        return max;
+    }
+    
 }
