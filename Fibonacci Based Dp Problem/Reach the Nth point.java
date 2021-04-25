df9bd4db5b5f20ba71b53dbdb22f98297fb16703
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2021-04-25 12:04:39 +0530

    Create Reach the Nth point.java

---
+++ Fibonacci Based Dp Problem/Reach the Nth point.java
@@ -0,0 +1,26 @@
+class Solution
+{
+    public int nthPoint(int n)
+    {
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,-1);
+        return countWay(n,dp);
+    }
+    public int countWay(int n,int dp[])
+    {
+        
+        if(n<=1)
+        {
+            return 1;
+        }
+        if(dp[n]!=-1)
+        {
+            return dp[n];
+        }
+        int option1=countWay(n-1,dp);
+        int option2=countWay(n-2,dp);
+        return dp[n]=(option1+option2);
+    }
+    
+    
+}
