272dc67efd28b036d9221416f0f53dd9a88872a1
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2021-04-29 21:39:37 +0530

    Create House Robber.java

---
+++ House Robber Based Question/House Robber.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int rob(int[] nums) {
+        int dp[]=new int[nums.length];
+        Arrays.fill(dp,-1);
+        return maxAmount(nums,0,dp);   
+    }
+    public int maxAmount(int wealth[],int currIndex,int dp[])
+    {
+        if(currIndex>=wealth.length)
+        {
+            return 0;
+        }
+        if(dp[currIndex]!=-1)
+        {
+            return dp[currIndex];
+        }
+        int stealCurrent=wealth[currIndex]+maxAmount(wealth,currIndex+2,dp);
+        int skipCurrent=maxAmount(wealth,currIndex+1,dp);
+        return dp[currIndex]=Math.max(stealCurrent,skipCurrent);
+    }
+}
