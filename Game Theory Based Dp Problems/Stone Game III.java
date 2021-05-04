5e7669c2796ce36e60dc751c98c132451947c486
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2021-05-04 19:56:38 +0530

    Update Stone Game III.java

--- Game Theory Based Dp Problems/Stone Game III.java
+++ Game Theory Based Dp Problems/Stone Game III.java
@@ -2,41 +2,48 @@ class Solution {
     public String stoneGameIII(int[] stoneValue) {
         int dp[]=new int[stoneValue.length+1];
         Arrays.fill(dp,-1);
-       int alice= maxStone(stoneValue,0,dp);
-        if(alice>0)
+       int alice=maxStone(stoneValue,0,dp);
+        int sum=0;
+        for(int i=0;i<stoneValue.length;i++)
+        {
+            sum=sum+stoneValue[i];
+        }
+        if(alice>sum-alice)
         {
             return "Alice";
         }
-        if(alice==0)
+        if(alice<sum-alice)
         {
-            return "Tie";
+            return "Bob";
         }
-        return "Bob";
-        
+        return "Tie";
     }
     public int maxStone(int stoneValue[],int i,int dp[])
     {
-        if(i>=stoneValue.length)
-        {
-            return 0;
-        }
+      if(i>=stoneValue.length)
+      {
+          return 0;
+      }
         if(dp[i]!=-1)
         {
             return dp[i];
-        }
-        else
-        {
-        int ans=Integer.MIN_VALUE;
-        ans=Math.max(ans,stoneValue[i]-maxStone(stoneValue,i+1,dp));
-        if(i+1<stoneValue.length)
-        {
-        ans=Math.max(ans,stoneValue[i]+stoneValue[i+1]-maxStone(stoneValue,i+2,dp)); 
-        }
-        if(i+2<stoneValue.length)
-        {
-ans=Math.max(ans,stoneValue[i]+stoneValue[i+1]+stoneValue[i+2]-maxStone(stoneValue,i+3,dp));
-        }
-            return dp[i]=ans;   
-        }
+        } 
+    int score=0;
+score=stoneValue[i] + Math.min(maxStone(stoneValue,i + 2,dp), Math.min(maxStone(stoneValue,i + 3,dp), maxStone(stoneValue,i + 4,dp))); 
+        
+if (i + 1 < stoneValue.length) 
+score = Math.max(score, stoneValue[i] + stoneValue[i + 1] + Math.min(maxStone(stoneValue,i + 3,dp), Math.min(maxStone(stoneValue,i + 4,dp), maxStone(stoneValue,i + 5,dp))));
+        
+if (i + 2 < stoneValue.length) 
+score = Math.max(score, stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] + Math.min(maxStone(stoneValue,i + 4,dp), Math.min(maxStone(stoneValue,i + 5,dp), maxStone(stoneValue,i + 6,dp))));   
+      return dp[i]=score;  
+       
+                                           
     }
 }
+                                           
+                                           
+                                           
+                                           
+                                           
+   
