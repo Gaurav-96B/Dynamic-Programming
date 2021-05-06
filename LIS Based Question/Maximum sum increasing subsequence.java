8dbd6da56c7b62712e279a0ade561fd1a4a55cc9
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2021-05-06 20:57:14 +0530

    Update Maximum sum increasing subsequence.java

--- LIS Based Question/Maximum sum increasing subsequence.java
+++ LIS Based Question/Maximum sum increasing subsequence.java
@@ -1,44 +1,31 @@
-class GFG {
-	public static void main (String[] args) {
-	    int t;
-	    Scanner sc=new Scanner(System.in);
-	    t=sc.nextInt();
-	    sc.nextLine();
-	    while(t-->0)
+class Solution
+{
+	public int maxSumIS(int arr[], int n)  
+	{  
+	    int maxSumLis[]=new int[n];
+	    for(int i=0;i<arr.length;i++)
 	    {
-	        int n=sc.nextInt();
-	        int a[]=new int[n];
-	        for(int i=0;i<n;i++)
-	        {
-	            a[i]=sc.nextInt();
-	        }
-	        int table[]=new int[n];
-	        table[0]=a[0];
-	        for(int i=1;i<n;i++)
-	        {
-	            table[i]=a[i];
-	            for(int j=i-1;j>=0;j--)
-	            {
-	               if(a[i]>a[j]) 
-	               {
-	                   int maxSum=table[j]+a[i];
-	                   if(maxSum>table[i])
-	                   {
-	                       table[i]=maxSum;
-	                   }
-	               }
-	            }
-	        }
-	        int max=0;
-	        for(int i=0;i<n;i++)
-	        {
-	            if(table[i]>max)
-	            {
-	               max=table[i]; 
-	            }
-	        }
-	        System.out.println(max);
+	      maxSumLis[i]=arr[i];  
 	    }
-	    
+	    return maxSum(arr,maxSumLis);
+	}
+	public int maxSum(int arr[],int maxSumLis[])
+	{
+	    for(int i=1;i<arr.length;i++)
+	    {
+	        for(int j=i-1;j>=0;j--)
+	        {
+	            if(arr[i]>arr[j])
+	            {
+	                maxSumLis[i]=Math.max(maxSumLis[i],maxSumLis[j]+arr[i]);
+	            }
+	        }
+	    }
+	    int max=0;
+	    for(int i=0;i<arr.length;i++)
+	    {
+	        max=Math.max(max,maxSumLis[i]);
+	    }
+	    return max;
 	}
 }
