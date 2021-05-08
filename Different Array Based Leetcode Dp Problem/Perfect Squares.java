6bc62447960aca379b5a31a619fc347ed1be93ca
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2021-05-08 21:43:20 +0530

    Update Perfect Squares.java

--- Different Array Based Leetcode Dp Problem/Perfect Squares.java
+++ Different Array Based Leetcode Dp Problem/Perfect Squares.java
@@ -1,1 +1,25 @@
 
+class Solution {
+    public int numSquares(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return perfectSquare(n,dp);
+    }
+    private int perfectSquare(int n,int[] dp){
+        if(n == 0)
+        {
+            return 0;
+        }
+        if(dp[n]!=-1) 
+        {
+            return dp[n];
+        }
+        int min = Integer.MAX_VALUE;
+        for(int i = 1;i*i<=n;i++)
+        {
+            int x=perfectSquare(n-i*i,dp) + 1;
+            min=Math.min(x,min);
+        }
+        return dp[n]=min;
+    }
+}
