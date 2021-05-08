db56d361a4b8612032e7c010d81d8bdb22f93f4a
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2021-05-08 17:32:56 +0530

    Update Partition Equal Subset Sum.java

--- Different Array Based Leetcode Dp Problem/Partition Equal Subset Sum.java
+++ Different Array Based Leetcode Dp Problem/Partition Equal Subset Sum.java
@@ -1,1 +1,40 @@
 
+class Solution {
+    public boolean canPartition(int[] arr) {
+        int sum=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            sum=sum+arr[i];
+        }
+        if(sum%2!=0)
+        {
+            return false;
+        }
+       Boolean[][] dp=new Boolean[arr.length+1][sum/2];
+       return canBePartitioned(arr,0,0,sum/2,arr.length,dp);
+    }
+     boolean canBePartitioned(int[] arr,int start,int sum,int targetSum, int n,Boolean[][] dp){
+        if(start>=n)
+        {
+            return false;
+        }
+        
+        if(targetSum==sum)
+        {
+            return true;
+        }
+        if(targetSum<sum)
+        {
+            return false;
+        }
+
+        if(dp[start][sum]!=null)
+        {
+            return dp[start][sum];
+        }
+        boolean found=canBePartitioned(arr,start+1,sum+arr[start],targetSum,n,dp)
+                                                ||
+                            canBePartitioned(arr,start+1,sum,targetSum,n,dp);
+        return dp[start][sum]=found;
+    }
+}
