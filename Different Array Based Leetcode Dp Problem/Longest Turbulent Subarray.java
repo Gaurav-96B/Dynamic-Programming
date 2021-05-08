1e4df750ea9e6f6f8a14fe2b78d012f7a1c41974
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2021-05-08 22:08:38 +0530

    Update Longest Turbulent Subarray.java

--- Different Array Based Leetcode Dp Problem/Longest Turbulent Subarray.java
+++ Different Array Based Leetcode Dp Problem/Longest Turbulent Subarray.java
@@ -1,1 +1,83 @@
 
+class Solution {
+  public int maxTurbulenceSize(int A[]) {
+        if(A.length==0) 
+        {
+            return 0;
+        }
+        int max = 1;
+        int curmax=1;
+        boolean now = true;
+        for(int i=1;i<A.length;i++)
+        {
+            if(i%2==0)
+            {
+                if(A[i]<A[i-1])
+                {
+                    if(now)
+                    {
+                     curmax=curmax+1;  
+                    }
+                    else
+                    {
+                    curmax=2;
+                    }
+                    now=true;
+                    max = Math.max(curmax,max);
+                }
+                else if(A[i]>A[i-1])
+                {
+                    if(!now)
+                    {
+                     curmax=curmax+1;  
+                    }
+                    else
+                    {
+                    curmax=2;
+                    }
+                    now=false;
+                    max = Math.max(curmax,max);
+                } 
+                else 
+                {
+                    curmax=1;
+                    continue;
+                }
+            }
+            else 
+            {
+                if(A[i]>A[i-1])
+                {
+                    if(now)
+                    {
+                     curmax=curmax+1;  
+                    }
+                    else
+                    {
+                    curmax=2;
+                    }
+                    now=true;
+                    max = Math.max(curmax,max);
+                }
+                else if(A[i]<A[i-1])
+                {
+                    if(!now)
+                    {
+                     curmax=curmax+1;  
+                    }
+                    else
+                    {
+                    curmax=2;
+                    }
+                    now=false;
+                    max = Math.max(curmax,max); 
+                } 
+                else {
+                    curmax=1;
+                    continue;
+                }
+            }
+        }
+        return max;
+    }
+}  
