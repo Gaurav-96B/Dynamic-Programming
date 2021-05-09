5dde819152d0f9d2c13e74135fe60560a3a16e48
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2021-05-09 11:23:16 +0530

    Update 2 Keys Keyboard.java

--- Different Array Based Leetcode Dp Problem/2 Keys Keyboard.java
+++ Different Array Based Leetcode Dp Problem/2 Keys Keyboard.java
@@ -1,1 +1,19 @@
-
+class Solution {
+    public int minSteps(int n) {
+	int res = 0;
+	if (n == 1) 
+    {
+        return res;  
+    }
+	int sqrtMid = (int) Math.sqrt(n);
+	for (int i = 2; i <= sqrtMid; i++) 
+    {
+		if (n % i == 0) 
+        { 
+			res=res+(i + minSteps(n / i)); 
+			return res;
+		}
+	}
+	return n; 
+}
+}
