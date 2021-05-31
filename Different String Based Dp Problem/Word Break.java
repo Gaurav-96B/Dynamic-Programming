631d9013b5d693dcadf28b703b46ef4c1c6444e4
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2021-05-31 17:11:23 +0530

    Update Word Break.java

--- Different String Based Dp Problem/Word Break.java
+++ Different String Based Dp Problem/Word Break.java
@@ -1,1 +1,31 @@
-
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String>set=new HashSet<>();
+        Boolean dp[]=new Boolean[s.length()+1];
+        for(int i=0;i<wordDict.size();i++)
+        {
+            set.add(wordDict.get(i));
+        }
+        return wordBreak(s,set,0,dp);   
+    }
+    public boolean wordBreak(String s,Set<String>set,int i,Boolean dp[])
+    {
+        if(i==s.length())
+        {
+            return true;
+        }
+        if(dp[i]!=null)
+        {
+            return dp[i];
+        }
+        for(int j=i+1;j<=s.length();j++)
+        {
+            String sub=s.substring(i, j);
+            if (set.contains(sub) && wordBreak(s,set,j,dp)) 
+            {
+                return dp[i]=true;
+            }
+        }
+        return dp[i]=false;
+    }
+}
