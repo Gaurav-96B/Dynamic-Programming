d811b944c734094e911cad6dc69818e76ef5804e
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2021-06-06 13:30:00 +0530

    Create Minimum number of deletions.java

---
+++ LPS Based Dp Problem/Minimum number of deletions.java
@@ -0,0 +1,36 @@
+class Solution 
+{ 
+    int minDeletions(String S, int n)
+    {
+        int dp[][]=new int[S.length()+1][S.length()+1];
+        for(int row[]:dp)
+        {
+            Arrays.fill(row,-1);
+        }
+        int LPS=LPS(S,0,S.length()-1,dp);
+        return S.length()-LPS;
+    }
+    static int LPS(String s1,int i,int j,int dp[][])
+    {
+        if(i>j)
+        {
+            return 0;
+        }
+        if(i==j)
+        {
+            return 1;
+        }
+        if(dp[i][j]!=-1)
+        {
+            return dp[i][j];
+        }
+        if(s1.charAt(i)==s1.charAt(j))
+        {
+            return dp[i][j]=2+LPS(s1,i+1,j-1,dp);
+        }
+        else
+        {
+          return dp[i][j]=Math.max(LPS(s1,i+1,j,dp),LPS(s1,i,j-1,dp));
+        }
+    }
+} 
