429ae881b4356fe9b9c859c21845eda497e9b94f
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2021-06-10 23:57:13 +0530

    Create Maximal Square.java

---
+++ Grid or Matrix Based Dp Problem/Maximal Square.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int dp[][]=new int[matrix.length+1][matrix[0].length+1];
+        for(int row[]:dp)
+        {
+            Arrays.fill(row,-1);
+        }
+        int ans=0;
+        int currMax=0;
+        for(int i=0;i<matrix.length;i++)
+        {
+            for(int j=0;j<matrix[0].length;j++)
+            {
+                if(matrix[i][j]=='1')
+                {
+                    currMax=rectangle(matrix,i,j,dp);
+                    ans=Math.max(ans,currMax);
+                    
+                }
+            }
+        }
+        return ans*ans;
+        
+    }
+    public int rectangle(char matrix[][],int i,int j,int dp[][])
+    {
+        if(i<0||j<0||i>=matrix.length||j>=matrix[0].length||matrix[i][j]=='0')
+        {
+            return 0;                                                           
+        }
+        if(dp[i][j]!=-1)
+        {
+            return dp[i][j];
+        }
+        int ans=0;
+        int right=rectangle(matrix,i,j+1,dp); 
+        int down=rectangle(matrix,i+1,j,dp);
+        int diagonal=rectangle(matrix,i+1,j+1,dp);
+        return dp[i][j]=1+Math.min(right,Math.min(down,diagonal));                                                              
+    }
+}
