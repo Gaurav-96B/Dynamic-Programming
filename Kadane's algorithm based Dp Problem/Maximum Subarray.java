f34678f64423d257863a54d277ef3245520ee44e
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2021-12-08 08:19:34 +0530

    Update Maximum Subarray.java

--- Kadane's algorithm based Dp Problem/Maximum Subarray.java
+++ Kadane's algorithm based Dp Problem/Maximum Subarray.java
@@ -1,1 +1,14 @@
-
+ public int maxSubArray(int[] nums) {
+        int sum=0;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++)
+        {
+            sum=sum+nums[i];
+            max=Math.max(max,sum);
+            if(sum<0)
+            {
+                sum=0;
+            }
+        }
+        return max;  
+    }
